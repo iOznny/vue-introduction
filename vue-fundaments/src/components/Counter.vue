b3d5f648d3035dc36aa5e7b116688a984295a08d
iOznny <f000170014@gmail.com>
2022-03-28 16:10:06 -0600

    Init first app in vue

---
+++ vue-fundaments/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Counter</h2>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+        data() {
+            return {
+                counter: 5
+            }
+        }
+    }
+</script>
+
+<style></style>
